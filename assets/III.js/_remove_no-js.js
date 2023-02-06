f07b0de69de133c3941fa8b73c4de8049cb65707
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-02-06 16:25:41 -0300

    .

---
+++ assets/III.js/_remove_no-js.js
@@ -0,0 +1,6 @@
+// #Remove_no-js
+
+// 1. Remove a classe ".no-js" do :root
+const NO_JS = document.querySelector(".no-js"); /* [1] */
+NO_JS.classList.remove("no-js"); /* [1] */
+// --------------------
