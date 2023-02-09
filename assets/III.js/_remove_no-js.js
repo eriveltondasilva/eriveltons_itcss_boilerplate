c3a39e0c6a5973799bded1ea309f0bf1c96e7fce
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-02-09 12:00:50 -0300

    .

--- assets/III.js/_remove_no-js.js
+++ assets/III.js/_remove_no-js.js
@@ -1,6 +1,7 @@
 // #Remove_no-js
 
-// 1. Remove a classe ".no-js" do :root
-const NO_JS = document.querySelector(".no-js"); /* [1] */
-NO_JS.classList.remove("no-js"); /* [1] */
-// --------------------
+// 1. Constante para guardar a referência do html
+// 2. Remove a classe ".no-js" do :root
+const NO_JS = document.querySelector("html"); /* [1] */
+NO_JS.classList.remove("no-js"); /* [2] */
+
--- assets/III.js/main.js
+++ assets/III.js/main.js
@@ -1,33 +1,39 @@
 // #Dark Mode:
 
-// function(): mudar para dark-mode
-function changeMode() {
-    $("body").toggleClass("dark-mode");
-};
-
-// function(): salvar ou remover dark-mode
-function saveMode() {
-    if ($("body").hasClass("dark-mode")) {
-        localStorage.setItem("dark-mode", 1);
-    } else {
-        localStorage.removeItem("dark-mode");
-    };
-};
-
-// function(): Carregar dark mode
-function loadMode() {
-    const darkMode = localStorage.getItem("dark-mode");
-
-    if (darkMode) {
-        changeMode();
-    };
-};
-
-
-// #
-$("#change-mode").click(() => {
-    changeMode();
-    saveMode();
+$(".js-dark-mode").click(() => {
+    $("html").toggleClass("dark");
 });
 
-loadMode();
+
+
+// // function(): mudar para dark-mode
+// function changeMode() {
+//     $("body").toggleClass("dark-mode");
+// };
+
+// // function(): salvar ou remover dark-mode
+// function saveMode() {
+//     if ($("body").hasClass("dark-mode")) {
+//         localStorage.setItem("dark-mode", 1);
+//     } else {
+//         localStorage.removeItem("dark-mode");
+//     };
+// };
+
+// function(): Carregar dark mode
+// function loadMode() {
+//     const darkMode = localStorage.getItem("dark-mode");
+
+//     if (darkMode) {
+//         changeMode();
+//     };
+// };
+
+
+// 
+// $("#change-mode").click(() => {
+//     changeMode();
+//     saveMode();
+// });
+
+// loadMode();
